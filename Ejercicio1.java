9470f6e2eb02477a0419e81884628c4d804af7e6
Estrella Melissa Orellana Cruz <orellanaestrella66@gmail.com>
2025-05-25 17:41:56 -0600

    Cambios

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -3,26 +3,21 @@ llamando “frutas” y que posteriormente muestre el contenido del vector pero 
 inversa, es decir, el último elemento ingresado deberá aparecer primero y el primero 
 en el último lugar.*/
 
+import java.util.Scanner;
 
 public class Ejercicio1 {
     
     public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
         String[] frutas = new String[10];
-        
-        // Llenar el vector con nombres de frutas
-        frutas[0] = "Manzana";
-        frutas[1] = "Banana";
-        frutas[2] = "Naranja";
-        frutas[3] = "Fresa";
-        frutas[4] = "Kiwi";
-        frutas[5] = "Mango";
-        frutas[6] = "Pera";
-        frutas[7] = "Uva";
-        frutas[8] = "Piña";
-        frutas[9] = "Melón";
 
-        // Mostrar el contenido del vector en forma inversa
-        for (int i = 9; i >= 0; i--) {
+        for (int i = 0; i < frutas.length; i++) {
+            System.out.print("Ingrese el nombre de la fruta " + (i + 1) + ": ");
+            frutas[i] = sc.nextLine();
+        }
+
+        System.out.println("\nFrutas en orden inverso:");
+        for (int i = frutas.length - 1; i >= 0; i--) {
             System.out.println(frutas[i]);
         }
     }
